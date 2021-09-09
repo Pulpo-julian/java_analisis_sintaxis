ad723db4116b80bc30918ffa743005484516daea
Pulpo-julian <julian.schrodinger@gmail.com>
2021-09-08 23:53:28 -0500

    introduzco codigo

---
+++ ensayo_01.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+
+public class ensayo_01 {
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ingrese un numero int: ");
+        int numero = sc.nextInt();
+        System.out.println("ingrese una palabra: ");
+        String palabra = sc.nextLine();
+        System.out.println("ingrese otra palabra: ");
+        String palabra2 = sc.nextLine();
+        
+        System.out.println("lo que ingreso fue: " + numero + ", " + palabra + ", " + palabra2);
+        
+        
+    }
+}
